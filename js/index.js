ad824a3beb2f38f3ac98298da5370148dee68ce2
Vitor Riboli <vitorriboli1@gmail.com>
2022-06-05 08:24:47 -0300

    game-over

--- js/index.js
+++ js/index.js
@@ -17,17 +17,23 @@ const loop = setInterval(function() {
     console.log(pipePosition)
     console.log(marioPosition)
 
-    if (pipePosition <= 120) {
-        
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`
+//Se a posição do pipe for <= 120px e menor que 0 e a posição do mario for menor que 80px
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
+        pipe.style.animation = 'none'; //para a animação do pipe
+        pipe.style.left = `${pipePosition}px` //para onde na posição que ele se encontra
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = "./images/game-over.png" //a imagem de game-over aparece
+        mario.style.width = '80px' // styles para a img game-over
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop); // limpar o loop quando perde o game
     }
 
 
 }, 10)
 
 document.addEventListener('keydown', jump);
-
-
-// parei o video em 23min
